73fd606ec53434d18d0a75f3c5144d5858400574
ahmed <ahmed.ibrahim6894@gmail.com>
2023-01-10 10:02:58 +0200

    +SplashScreen

--- lib/ui/SplashView.dart
+++ lib/ui/SplashView.dart
@@ -1,14 +1,42 @@
 
+import 'dart:async';
+
 
 import 'package:flutter/material.dart';
+import 'package:news_app/ui/HomeScreen.dart';
 
 class SplashVeiw extends StatelessWidget{
   static const String routeName = 'splash';
   @override
   Widget build(BuildContext context) {
 
-    return Container(
+    Timer(Duration(seconds: 3) , (){
+      Navigator.of(context).pushReplacementNamed(HomeScreen.routeName);
+    });
 
+    return Stack(
+      children: [
+
+        Container(
+          color: Colors.white,
+          child: Image.asset("assets/images/pattern.png" ,
+          width: double.infinity,
+          height: double.infinity,
+            fit: BoxFit.fill,
+          ),
+        ) ,
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Container(
+
+              child: Image.asset("assets/images/logo.png"),
+            ),
+          ),
+        )
+
+
+      ],
     );
 
   }
