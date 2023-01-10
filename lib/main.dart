3f13ecd2e40e1c9f2cc806f673b2b974bd9130c8
ahmed <ahmed.ibrahim6894@gmail.com>
2023-01-10 09:23:35 +0200

    init ui

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/ui/HomeScreen.dart';
+import 'package:news_app/ui/SplashView.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+
+      theme: ThemeData(
+        primarySwatch: Colors.green
+      ),
+
+    initialRoute: SplashVeiw.routeName,
+
+
+
+    routes:{
+      SplashVeiw.routeName : (context) => SplashVeiw() ,
+      HomeScreen.routeName : (context) => HomeScreen() ,
+
+
+
+    } ,
+    );
+
+
+  }
+}
---
+++ lib/ui/SplashView.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+class SplashVeiw extends StatelessWidget{
+  static const String routeName = 'splash';
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+
+    );
+
+  }
+
+}
